ee5de3a81ed641df84f0e17670606910d588a413
Omkar-OP <omkarbharate22@gmail.com>
2026-01-29 13:40:08 +0530

    new one

--- cloudConfig.js
+++ cloudConfig.js
@@ -3,9 +3,9 @@ const { application } = require("express");
 const {CloudinaryStorage} = require("multer-storage-cloudinary")
 
 cloudinary.config({
-    cloud_name:process.env.CLOUD_NAME,
-    api_key:process.env.CLOUD_API_KEY,
-    api_secret:process.env.CLOUD_API_SECRET
+    cloud_name:process.env.CLOUDINARY_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_API_SECRET
 })
 
 const storage = new CloudinaryStorage({
